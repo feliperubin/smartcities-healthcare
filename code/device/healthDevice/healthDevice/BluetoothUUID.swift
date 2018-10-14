05617087ea53f3841a0495722cfa625f59508d08
Felipe Rubin <felipeprubin@gmail.com>
2018-10-14 20:10:26 -0300

    Starting iBeacon change

--- code/device/healthDevice/healthDevice/BluetoothUUID.swift
+++ code/device/healthDevice/healthDevice/BluetoothUUID.swift
@@ -11,13 +11,14 @@
  https://github.com/stowersjoshua/iOS-Swift-4-BLE-Example/blob/master/Basic%20Chat/UUIDKey.swift
 */
 import CoreBluetooth
-
-let kBLEService_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+//On Terminal: uuidgen
+let beacon_uuid = "AC000E32-4751-4959-B5E5-7FF4759861A8"
+//let beacon_major: 
 let kBLE_Characteristic_uuid_Tx = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 let kBLE_Characteristic_uuid_Rx = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 let MaxCharacters = 20
 
-let BLEService_UUID = CBUUID(string: kBLEService_UUID)
+//let BLEService_UUID = CBUUID(string: kBLEService_UUID)
 let BLE_Characteristic_uuid_Tx = CBUUID(string: kBLE_Characteristic_uuid_Tx)//(Property = Write without response)
 let BLE_Characteristic_uuid_Rx = CBUUID(string: kBLE_Characteristic_uuid_Rx)// (Property = Read/Notify)
 
--- code/device/healthDevice/healthDevice/ViewController.swift
+++ code/device/healthDevice/healthDevice/ViewController.swift
@@ -8,41 +8,81 @@
 
 import UIKit
 import HealthKit
-import Foundation
+import Foundation //Don't know if this is required
 import WatchConnectivity
-class ViewController: UIViewController, WCSessionDelegate {
+
+import CoreLocation
+import CoreBluetooth
+/*
+ https://www.hackingwithswift.com/example-code/location/how-to-make-an-iphone-transmit-an-ibeacon
+*/
+class ViewController: UIViewController, WCSessionDelegate, CBPeripheralManagerDelegate {
     
+    /////////////////////////////WATCH/////CONNECTIVITY/////////////////////////////////////////////
     var session: WCSession?
-    
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?){}
     func sessionDidBecomeInactive(_ session: WCSession) {}
     func sessionDidDeactivate(_ session: WCSession) {}
     func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
-        DispatchQueue.main.async {
-            self.processConnectivity()
-        }
+        DispatchQueue.main.async {self.processConnectivity()}
     }
-    
    func processConnectivity() {
         print("iOS Connectivity Async")
         if let watchosContext = self.session?.receivedApplicationContext as? [String: String] {
             self.heartRateLabel.text = watchosContext["hr"]
         }
     }
-
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    let locationManager = CLLocationManager()
+    var localBeacon: CLBeaconRegion!
+    var beaconPeripheralData: NSDictionary!
+    var peripheralManager: CBPeripheralManager!
+    private func initLocalBeacon() {
+        if localBeacon != nil {
+            stopLocalBeacon()
+        }
+        let localBeaconUUID = "6486A0F0-BBA6-4937-9079-3E0344CC98EE"
+        let localBeaconMinor: CLBeaconMinorValue = 456
+        let localBeaconMajor: CLBeaconMajorValue = 123
+        let uuid = UUID(uuidString: localBeaconUUID)!
+        let localBeaconID = "TestBeacon"
+        localBeacon = CLBeaconRegion(proximityUUID: uuid, major: localBeaconMajor, minor: localBeaconMinor, identifier: localBeaconID)
+        beaconPeripheralData = localBeacon.peripheralData(withMeasuredPower: nil)
+        peripheralManager = CBPeripheralManager(delegate: self,queue: nil, options: nil)
+    }
+    private func stopLocalBeacon() {
+        peripheralManager.stopAdvertising()
+        peripheralManager = nil
+        beaconPeripheralData = nil
+        localBeacon = nil
+    }
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn {
+            peripheralManager.startAdvertising(beaconPeripheralData as! [String: AnyObject])
+        } else if peripheral.state == .poweredOff {
+            peripheralManager.stopAdvertising()
+        }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var heartRateLabel: UILabel!
-    
+    var cbRegion : CLBeaconRegion?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Required for WatchConnectivity
         if(WCSession.isSupported()) {
             session = WCSession.default
             session?.delegate = self
             session?.activate()
-        } else{
-            print("iOS WCSession not Supported")
         }
+        //Required for Beacon
+        locationManager.requestAlwaysAuthorization()
+
+    //        advertiseBeacon(region: createBeaconRegion()!)
+        initLocalBeacon()
+        
     }
     
     @IBAction func startButtonAction(_ sender: Any) {
